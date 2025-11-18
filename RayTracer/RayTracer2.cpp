356bb126cf3f5e6d4c5956538c6e1d5db5aa6727
AlexandreBasei <111810502+AlexandreBasei@users.noreply.github.com>
2025-11-18 12:02:39 +0100

    images

--- RayTracer/RayTracer2.cpp
+++ RayTracer/RayTracer2.cpp
@@ -78,28 +78,19 @@ void bouncing_spheres() {
     cam.render(world);
 }
 
-void triangles() {
+void checkered_spheres() {
     hittable_list world;
 
-    auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
-    world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
+    auto checker = make_shared<checker_texture>(0.32, color(.2, .3, .1), color(.9, .9, .9));
 
-    auto material1 = make_shared<dielectric>(1.5);
-    world.add(make_shared<triangle>(point3(0, 1, 0), point3(-1, 0, 0), point3(1, 0, 0), material1));
-
-    /*auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
-    world.add(make_shared<triangle>(point3(-4, 1, 0), 1.0, material2));
-
-    auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
-    world.add(make_shared<triangle>(point3(4, 1, 0), 1.0, material3));*/
-
-    world = hittable_list(make_shared<bvh_node>(world));
+    world.add(make_shared<sphere>(point3(0, -10, 0), 10, make_shared<lambertian>(checker)));
+    world.add(make_shared<sphere>(point3(0, 10, 0), 10, make_shared<lambertian>(checker)));
 
     camera cam;
 
     cam.aspect_ratio = 16.0 / 9.0;
     cam.image_width = 400;
-    cam.samples_per_pixel = 50;
+    cam.samples_per_pixel = 100;
     cam.max_depth = 50;
 
     cam.vfov = 20;
@@ -107,14 +98,37 @@ void triangles() {
     cam.lookat = point3(0, 0, 0);
     cam.vup = vec3(0, 1, 0);
 
-    cam.defocus_angle = 0.6;
-    cam.focus_dist = 10.0;
+    cam.defocus_angle = 0;
 
     cam.render(world);
 }
 
-int main() {
-    //bouncing_spheres();
-    triangles();
+void earth() {
+    auto earth_texture = make_shared<image_texture>("earthmap.jpg");
+    auto earth_surface = make_shared<lambertian>(earth_texture);
+    auto globe = make_shared<sphere>(point3(0, 0, 0), 2, earth_surface);
 
+    camera cam;
+
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width = 400;
+    cam.samples_per_pixel = 100;
+    cam.max_depth = 50;
+
+    cam.vfov = 20;
+    cam.lookfrom = point3(0, 0, 12);
+    cam.lookat = point3(0, 0, 0);
+    cam.vup = vec3(0, 1, 0);
+
+    cam.defocus_angle = 0;
+
+    cam.render(hittable_list(globe));
+}
+
+int main() {
+    switch (3) {
+    case 1:  bouncing_spheres();  break;
+    case 2:  checkered_spheres(); break;
+    case 3:  earth();             break;
+    }
 }
